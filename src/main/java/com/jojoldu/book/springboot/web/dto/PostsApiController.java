ab66dbd3e937af3807654f3dcac2f7fdc2455759
ParkHongJin <ektkfk180@naver.com>
2022-01-06 20:43:56 +0900

    수정/조회 메서드 추가

--- src/main/java/com/jojoldu/book/springboot/web/dto/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsApiController.java
@@ -15,5 +15,14 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
-    
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
 }
