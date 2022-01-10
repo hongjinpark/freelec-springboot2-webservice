4454f3166e49c99d087e1eef7015c213dd64504d
ParkHongJin <ektkfk180@naver.com>
2022-01-10 21:11:41 +0900

    index 메소드 model 값 추가

--- src/main/java/com/jojoldu/book/springboot/web/dto/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/IndexController.java
@@ -1,8 +1,10 @@
 package com.jojoldu.book.springboot.web.dto;
 
 
+import com.jojoldu.book.springboot.domain.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
